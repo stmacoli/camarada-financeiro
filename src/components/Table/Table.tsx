b1a1cca86730d812480b19995ac79418962499e7
StMacoli <st.macoli@gmail.com>
2022-07-20 16:01:26 -0300

    Table Row Dinamico

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,28 @@
-type TableItem = {
-    nome: string,
-    categoria: string,
-    valor: number,
-    data: string
-}
+import { FC } from "react"
+
+type TableItem  = {
+  nome?: string;
+  categoria?: string;
+  valor?: number;
+  data?: string;
+};
+
+const TableRow : FC<TableItem> = ({nome, categoria, valor, data}) => {
+  return (
+    
+    <tr>
+      <th>{data}</th>
+      <td>{nome}</td>
+      <td>{categoria}</td>
+      <td>{valor}</td>
+    </tr>
+  );
+};
+
+
 type TableProps = {
-    data: TableItem[]
-}
+  data: TableItem[];
+};
 
 const Table = () => {
   return (
@@ -21,12 +37,7 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
+          <TableRow nome="burguer king" categoria="Refeição" data="20/10/2022" valor={169.69}/>
           <tr>
             <th>2</th>
             <td>Jacob</td>
