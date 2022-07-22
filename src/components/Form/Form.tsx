27d896f0427b68aac07160df1eb3b663c2aa8c92
StMacoli <st.macoli@gmail.com>
2022-07-22 12:58:14 -0300

    Ajustes component form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,35 +1,57 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [data, setData] = useState("");
+  const [nome, setNome] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [valor, setValor] = useState(0);
+
   return (
     <form>
       <div>
         <label>Data</label>
         <div>
           {/* CASO DÊ MTO TRABALHO, PODEMOS MUDAR ESSE TYPE PARA TEXT */}
-          <input type={"datetime-local"}/>
+          <input
+            type="text"
+            onChange={(event) => setData(event.target.value)}
+            value={data}
+            placeholder="Selecionar"
+          />
         </div>
       </div>
       <div>
         <label>Nome da Entrada</label>
         <div>
-          <input type="text" placeholder="Dispensa do Mercado" />
+          <input
+            type="text"
+            onChange={(event) => setNome(event.target.value)}
+            value={nome}
+            placeholder="Dispensa do Mercado"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Categoria</label>
+        <div>
+          <input
+            type="text"
+            onChange={(event) => setCategoria(event.target.value)}
+            value={categoria}
+            placeholder="Propheta Pizzaria"
+          />
         </div>
       </div>
 
       <div>
         <label>Valor</label>
         <div>
-          <input type="number" placeholder="R$" />
+          <input
+            type="number"
+            onChange={(event) => setValor(Number(event.target.value))}
+            placeholder="R$"
+          />
         </div>
-      </div>
-
-      <div>
-        <label>Categorias</label>
-        <select>
-          <option selected>Expandir</option>
-          <option value="1">Mercado</option>
-          <option value="2">Lazer</option>
-          <option value="3">Música</option>
-        </select>
       </div>
 
       <div>
