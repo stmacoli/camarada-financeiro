ce4edaf31663d160ab5ef26ea8473db340941898
StMacoli <st.macoli@gmail.com>
2022-07-27 23:33:02 -0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,36 @@
-import Header from "./components/Header/Header";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+
+import Header from "./components/Header/Header";
 import Table from "./components/Table/Table";
 import Form from "./components/Form/Form";
-import React, { useState } from "react";
 
-const TABLE_MOCK_DATA = [
-  {
-    nome: "Mercado",
-    categoria: "Casa",
-    valor: 239.55,
-    data: "20/10/2034",
-    id: "1",
-  },
-  {
-    nome: "Sex Shop",
-    categoria: "Lazer",
-    valor: 669.69,
-    data: "20/10/2034",
-    id: "2",
-  },
-  {
-    nome: "Gibson Flying V",
-    categoria: "Música",
-    valor: 14239.35,
-    data: "21/10/2034",
-    id: "3",
-  },
-  {
-    nome: "Estudos",
-    categoria: "Investimentos",
-    valor: 3239.55,
-    data: "22/10/2034",
-    id: "4",
-  },
-];
+const ENDPOINT = "http://localhost:3000/entradas";
 
 const App = () => {
-  const [tableData, setTableData] = useState(TABLE_MOCK_DATA);
+  const [tableData, setTableData] = useState([]);
+  const [index, setIndex] = useState(41654)
+
+  useEffect(() => {
+    axios.get(ENDPOINT).then((response) => {
+      console.log(response.data);
+      setTableData(response.data);
+    });
+  }, []);
 
   const handleSubmit = (entrada: any) => {
     let newArray = [...tableData, entrada];
     setTableData(newArray);
-    return "Sucesso!"
+    return "Sucesso!";
   };
 
   return (
     <div className="container">
+      <h1>{index}</h1>
+      <button onClick={() => setIndex(index + 1)}>+ 1</button>
       <Header> Camarada Financeiro </Header>
-      <Form onSubmit={handleSubmit}/>
+      <Form onSubmit={handleSubmit} />
       <Table data={tableData} />
     </div>
   );
